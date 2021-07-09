3e6f351c57623f19adf9d45968aefd727f8e4215
DanielMamaev <mamaev.s.d@gmail.com>
2021-07-09 23:50:42 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
 
-rtklib_v = "RTKLIB_2.4.3_b33"
+rtklib_v = "RTKLIB-2.4.3-b33"
 ver = "v2.4.3-b33"
+
 os.system("sudo apt install python3-pip -y")
 os.system("sudo apt install python3-pyqt5 -y")
 os.system("sudo apt install xterm -y")
@@ -13,9 +14,10 @@ os.system("pip3 install ftputil")
 os.system("pip3 install psutil")
 os.system("sudo apt install unzip -y")
 if not os.path.exists(os.getcwd() + "/" + rtklib_v + ".zip") and not os.path.exists(os.getcwd() + "/" + rtklib_v):
-    os.system("wget https://github.com/tomojitakasu/RTKLIB/releases/download/" + ver + "/" + rtklib_v +".zip")
-    os.system("unzip RTKLIB_2.4.3_b33")
-    os.system("cd RTKLIB_2.4.3_b33/lib/iers/gcc; make")
-    os.system("cd RTKLIB_2.4.3_b33/app/convbin/gcc; make")
-    os.system("cd RTKLIB_2.4.3_b33/app/rnx2rtkp/gcc; make")
-    os.system("cd RTKLIB_2.4.3_b33/app/str2str/gcc; make")
+    os.system("wget https://github.com/tomojitakasu/RTKLIB/archive/refs/tags/" + ver +".zip")
+    os.system("unzip "+ ver +".zip")
+os.system("cd " + rtklib_v + "/lib/iers/gcc; make")
+os.system("cd " + rtklib_v + "/app/convbin/gcc; make")
+os.system("cd " + rtklib_v + "/app/rnx2rtkp/gcc; make")
+os.system("cd " + rtklib_v + "/app/str2str/gcc; make")
+print("DONE!")
