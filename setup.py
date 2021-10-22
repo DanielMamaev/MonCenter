cf9d1dbccc57316d0da7e5642afc5431d95169b5
DanielMamaev <mamaev.s.d@gmail.com>
2021-10-22 23:01:10 +0700

    add copy str2str -> str2str_auto for auto post

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
 
+
 rtklib_v = "RTKLIB-2.4.3-b33"
 ver = "v2.4.3-b33"
+
+
 
 os.system("sudo apt install python3-pip -y")
 os.system("sudo apt install python3-pyqt5 -y")
@@ -16,11 +19,13 @@ os.system("pip3 install psutil")
 os.system("sudo apt install unzip -y")
 os.system('pip install folium')
 os.system('pip install PyQtWebEngine')
-if not os.path.exists(os.getcwd() + "/" + rtklib_v + ".zip") and not os.path.exists(os.getcwd() + "/" + rtklib_v):
+if not os.path.exists(os.getcwd() + "/" + ver + ".zip") and not os.path.exists(os.getcwd() + "/" + rtklib_v):
     os.system("wget https://github.com/tomojitakasu/RTKLIB/archive/refs/tags/" + ver +".zip")
     os.system("unzip "+ ver +".zip")
 os.system("cd " + rtklib_v + "/lib/iers/gcc; make")
 os.system("cd " + rtklib_v + "/app/convbin/gcc; make")
 os.system("cd " + rtklib_v + "/app/rnx2rtkp/gcc; make")
 os.system("cd " + rtklib_v + "/app/str2str/gcc; make")
+os.system("cp " + os.getcwd() + "/" + rtklib_v + "/app/str2str/gcc/str2str " + os.getcwd() + "/" + rtklib_v + "/app/str2str/gcc/str2str_auto")
+
 print("\nDONE!")
